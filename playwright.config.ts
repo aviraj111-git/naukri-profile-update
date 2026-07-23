ac941ba5e08b8fb95acb9e42a751d36e72a0ca8a
Avinash Kumar <iamavi.111@gmail.com>
2026-07-23 12:45:47 +0530

    integrated report portal

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from '@playwright/test';
 import path from 'path';
+import {env} from './src/utility/env.js';
 
 const authFile = path.join(process.cwd(), 'creds/.auth/webLocalData.json');
 
+const rpConfig = {
+  apiKey: env.RP_UUID,
+  endpoint: 'https://demo.reportportal.io/api/v1',
+  project: 'default_personal',          
+  launch: 'Profile Update Test Execution',
+  attributes: [
+    { key: 'platform', value: 'windows 10' },
+    { value: 'demo' }
+  ],
+  mode: 'DEFAULT',
+  description: 'Automated E2E tests run via Playwright',
+};
+
 export default defineConfig({
   testDir: './tests',
+  reporter: [
+    ['html'], 
+    ['@reportportal/agent-js-playwright', rpConfig]
+  ],
   use: {
     baseURL: 'https://www.naukri.com',
     screenshot: 'only-on-failure',
